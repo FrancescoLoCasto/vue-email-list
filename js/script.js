594c58fec8d65889e7f452771d265a655973dabb
MyUsername <my@email.com>
2022-12-21 15:08:39 +0100

    Populated for get random mail

--- js/script.js
+++ js/script.js
@@ -10,7 +10,10 @@ createApp({
   methods: {
    getRandomMail(){
       for (let i = 0; i < 10; i++) {
-         
+         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+         .then((response) => {
+            this.mail.push(response.data.response)
+         })
       }
    }
   }
